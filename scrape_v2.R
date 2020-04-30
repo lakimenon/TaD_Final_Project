721a492380d08cbedce42026ae047203e02e6aaf
lakimenon <laki.menon@gmail.com>
2020-04-30 17:21:19 -0400

    Updated Script and added most texts

--- scrape_v2.R
+++ scrape_v2.R
@@ -2,6 +2,7 @@ rm(list = ls())
 library(rvest)
 library(dplyr)
 library(rjson)
+setwd("~/Documents/NYU/Spring2020/DSGA 1015/Project/TaD_Final_Project")
 
 #Parse JSON of urls of all covid related transcripts
 json_data <- fromJSON(file='WebscrapingCode/raw_content.json')
@@ -19,30 +20,55 @@ rownames(df) <- NULL
 #Correcting URLs
 url_prefix <-'https://www.rev.com/blog/transcripts/'
 titles <- gsub(' ', '-', df$title) %>% tolower()
+titles <- iconv(titles, 'utf-8', 'ascii', sub='')
+titles <- gsub(':', '', titles) 
 df$url <- Map(paste, url_prefix, titles, sep = "")
 
 #Selecting only transcripts of Country Leaders
-toMatch <- c('Trump', 'Trudeau', 'Morrison', 'Kingdom', 'Britain', 'Zealand', 'Boris', 'Task', 'U.S.')
+toMatch <- c('Trump', 'Trudeau', 'Morrison', 'Kingdom', 'Britain', 'Zealand', 'Boris', 'Task', 'U.S.', 'Jacinda', 'Ardern')
 matches <- df[(grepl(paste(toMatch,collapse='|'), 
                      df$title)), ]
 row.names(matches) <- NULL
 
-
-#Scraping required texts
-scrapetextonly <- function(url){
+scrapetextonly <- function(url, title){
   transcript <- read_html(url)
   text <- transcript %>% 
     rvest::html_nodes(xpath = "//div[contains(@class, 'fl-callout-text')]") %>% 
     xml2::xml_find_all("div/p/text()[preceding-sibling::br]") %>% 
     rvest::html_text()
+  
+  text_fname = paste(title, '.txt', sep = '')
+  text_fname = paste('Data/all_texts/', text_fname, sep = '')
+  write_file(text_fname, text)
   return(toString(text))
 }
 
-texts <- list()
-for(url in matches$url){
-  trans <- scrapetextonly(url)
-  texts <- append(texts, trans)
+write_file <- function(fname, data){
+  fileConn<-file(fname)
+  writeLines(data, fileConn)
+  close(fileConn)  
 }
 
-#Append transcript text to table with URL and title
-matches$texts <- texts
+try_scrape <- function(url, title) {
+  out <- tryCatch(
+    {
+      scrapetextonly(url, title)
+    },
+    error=function(cond) {
+      message(paste("URL does not seem to exist:", url))
+      message("Here's the original error message:")
+      message(cond)
+      return(NA)
+    }
+  )    
+  return(out)
+}
+
+errs <- c()
+for( i in 1:nrow(matches)){
+  trans <- try_scrape(toString(matches$url[i]), toString(matches$title[i]))
+  if(is.na(trans)){
+    errs <- append(errs, i)
+  }
+  texts <- append(texts, trans)
+}
